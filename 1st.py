8ccff84c7cad253f3574b01948cd8402a420d566
Deepak Kumar Bhuyan <163822427+Deepak7242@users.noreply.github.com>
2024-03-18 21:15:11 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,21 @@
+# a=50
+# print(type(a))
+# a=60
+#print(b(a))
+#a=345
+#print(oct(a))
+#print(bin(0o620))
+a=50+60j
+#print(type(a))
+#print(a.real)
+#print(a.imag)
+#a=True
+#b=false
+#print(a)
+#print(##b)
+#a=50
+#b=60
+#result=a<b
+#print(result)
+#c=True
+address="""""""
---
+++ 2nd.py
@@ -0,0 +1,3 @@
+A=float(input("enter a number"))
+B=float(input("entera number"))
+print(A+B)
